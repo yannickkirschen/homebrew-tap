916ea1544f4792e0906da2e76176d4f64c15bf0d
Yannick Kirschen <yannickkirschen@protonmail.com>
2024-05-06 17:59:40 +0200

    feat: initial commit

---
+++ Formulas/ddir/ddir.rb
@@ -0,0 +1,17 @@
+class Ddir < Formula
+    desc "Diff a directory and sync changes."
+    homepage "https://github.com/yannickkirschen/ddir"
+    url "https://github.com/yannickkirschen/ddir/archive/refs/tags/3.0.1.zip"
+    sha256 "bd7a9d052c1a726d3fd85489cee78e85bd421bad5f6f9108fb26a1da4d6e4e2b"
+    license :public_domain
+
+    depends_on "python@3.12"
+
+    def install
+        virtualenv_install_with_resources
+    end
+
+    test do
+      system "#{bin}/ddir", "--version"
+    end
+  end
---
+++ README.md
@@ -0,0 +1,8 @@
+# Brew formulas for ddir
+
+You can install [ddir](https://github.com/yannickkirschen/ddir) by executing:
+
+```bash
+brew tap yannickkirschen/ddir
+brew install ddir
+```
